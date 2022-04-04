d6d1f45c76172c82e11c9e668f9fe3fd19a663ee
Nick Adams <nick.pappas.adams@gmail.com>
2022-04-04 14:45:17 -0500

    added subtraction method in greetings package

--- hello/hello.go
+++ hello/hello.go
@@ -10,7 +10,10 @@ func main() {
 	message := greetings.Hello("Nick")
 	fmt.Println(message)
 
-	addition := greetings.Add(3, 7)
+	addition := greetings.Add(3, 6)
 	fmt.Println(addition)
+
+	subtraction := greetings.Subtract(10, 4)
+	fmt.Println(subtraction)
 
 }
